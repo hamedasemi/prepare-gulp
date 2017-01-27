a1801c49913664ce02c59b6a7931bc1635a347e3
Hamed Asemi <h-@live.se>
2017-01-27 13:50:02 +0100

    Font works

--- gulp/tasks/font.js
+++ gulp/tasks/font.js
@@ -3,6 +3,9 @@ import {debug} from 'webrew-helpers-log';
 import gulp, {task, watch, src, dest, parallel, series} from 'gulp';
 import {obj} from 'stream-combiner2';
 import gulpFont from 'gulp-font';
+import gulpTtf2eot from 'gulp-ttf2eot';
+import gulpTtf2woff from 'gulp-ttf2woff';
+import gulpTtf2woff2 from 'gulp-ttf2woff2';
 
 import config from './../config';
 
@@ -17,10 +20,31 @@ let fontConfig = {
     collate: false
 };
 
-export function font() {
-    return src(`${config.src}/assets/fonts/**/*.{ttf,otf}`, { read: false })
-        .pipe(gulpFont(fontConfig))
+export function fontTtf2Ttf() {
+    return src(`${config.src}/assets/fonts/**/*.{ttf}`)
         .pipe(dest(config.dest));
 }
+
+export function fontTtf2Eot() {
+    return src(`${config.src}/assets/fonts/**/*.{ttf}`)
+        .pipe(gulpTtf2eot())
+        .pipe(dest(config.dest));
+}
+
+export function fontTtf2woff() {
+    return src(`${config.src}/assets/fonts/**/*.{ttf}`)
+        .pipe(gulpTtf2woff())
+        .pipe(dest(config.dest));
+}
+
+export function fontTtf2woff2() {
+    return src(`${config.src}/assets/fonts/**/*.{ttf}`)
+        .pipe(gulpTtf2woff2())
+        .pipe(dest(config.dest));
+}
+
+export const font = parallel(
+    fontTtf2woff2, fontTtf2woff, fontTtf2Eot, fontTtf2Ttf
+)
 
 font.description = `Generate web font package from ttf and otf files.`;
