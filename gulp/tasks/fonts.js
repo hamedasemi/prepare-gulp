57ed7c4978647046870e9232f323cb276caf7455
Hamed <hamed@Sherrys-MacBook.lan>
2017-02-18 22:30:00 +0100

    simple fonts task

---
+++ gulp/tasks/fonts.js
@@ -0,0 +1,18 @@
+import { src, dest } from 'gulp';
+import { obj } from 'stream-combiner2';
+
+import newer from 'gulp-newer';
+import font from './../plugins/gulp-font';
+
+import config from './../config';
+
+export function fonts(done) {
+    obj([
+        src(`${config.src}/assets/fonts/**/*.ttf`),
+        // newer(config.dest),
+        font(),
+        dest(config.dest)
+
+    ]).on(`error`, console.error.bind(console));
+    done()
+}
